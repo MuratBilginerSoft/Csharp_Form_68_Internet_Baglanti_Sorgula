a99f4e6b95a07de1519ae04c98ed542073abd789
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 22:55:55 +0300

    Initial commit

---
+++ Kod H Internet Baglanti Sorgula/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Kod_H_Internet_Baglanti_Sorgula
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnSorgula = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.PicDurum = new System.Windows.Forms.PictureBox();
+            this.LblSonuç = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicDurum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnSorgula
+            // 
+            this.BtnSorgula.BackColor = System.Drawing.Color.Transparent;
+            this.BtnSorgula.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSorgula.ForeColor = System.Drawing.Color.White;
+            this.BtnSorgula.Location = new System.Drawing.Point(92, 84);
+            this.BtnSorgula.Name = "BtnSorgula";
+            this.BtnSorgula.Size = new System.Drawing.Size(145, 35);
+            this.BtnSorgula.TabIndex = 0;
+            this.BtnSorgula.Text = "Bağlantı Sorgula";
+            this.BtnSorgula.UseVisualStyleBackColor = false;
+            this.BtnSorgula.Click += new System.EventHandler(this.BtnSorgula_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Kod_H_Internet_Baglanti_Sorgula.Properties.Resources._23;
+            this.pictureBox1.Location = new System.Drawing.Point(265, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(21, 21);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::Kod_H_Internet_Baglanti_Sorgula.Properties.Resources._3;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 240);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(286, 36);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(75, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Industryolog Akademi";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Kod_H_Internet_Baglanti_Sorgula.Properties.Resources._01___Logo_2017;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(29, 28);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(48, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IA İnternet Bağlantısı Sorgula";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Kod_H_Internet_Baglanti_Sorgula.Properties.Resources.wifi_xxl;
+            this.pictureBox3.Location = new System.Drawing.Point(51, 84);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // PicDurum
+            // 
+            this.PicDurum.BackColor = System.Drawing.Color.Transparent;
+            this.PicDurum.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PicDurum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PicDurum.Location = new System.Drawing.Point(106, 125);
+            this.PicDurum.Name = "PicDurum";
+            this.PicDurum.Size = new System.Drawing.Size(76, 67);
+            this.PicDurum.TabIndex = 6;
+            this.PicDurum.TabStop = false;
+            // 
+            // LblSonuç
+            // 
+            this.LblSonuç.BackColor = System.Drawing.Color.Transparent;
+            this.LblSonuç.ForeColor = System.Drawing.Color.White;
+            this.LblSonuç.Location = new System.Drawing.Point(1, 203);
+            this.LblSonuç.Name = "LblSonuç";
+            this.LblSonuç.Size = new System.Drawing.Size(286, 34);
+            this.LblSonuç.TabIndex = 6;
+            this.LblSonuç.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Kod_H_Internet_Baglanti_Sorgula.Properties.Resources._15;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(288, 277);
+            this.Controls.Add(this.LblSonuç);
+            this.Controls.Add(this.PicDurum);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnSorgula);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicDurum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSorgula;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox PicDurum;
+        private System.Windows.Forms.Label LblSonuç;
+    }
+}
+
---
+++ Kod H Internet Baglanti Sorgula/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Kod_H_Internet_Baglanti_Sorgula
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSorgula_Click(object sender, EventArgs e)
+        {
+            Ping ping = new Ping();
+            PingReply pingstatü = ping.Send(IPAddress.Parse("208.69.34.231"));
+
+            if (pingstatü.Status == IPStatus.Success)
+            {
+                PicDurum.BackgroundImage = Image.FromFile("Wifi.png");
+                LblSonuç.Text = "Bağlantı Var";
+            }
+
+            else
+            {
+                PicDurum.BackgroundImage = Image.FromFile("NoWifi.jpg");
+                LblSonuç.Text = "Bağlantı Yok";
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
